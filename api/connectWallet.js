4eda571b9e858b167920b9d2aa53d383110cec65
Ed <keecheong123@hotmail.com>
2023-09-21 06:20:58 +0800

    ed

--- api/connectWallet.js
+++ api/connectWallet.js
@@ -1,1 +1,18 @@
- 
+const authenticationAdapter = require('./authenticationAdapter');
+
+module.exports = async (req, res) => {
+  if (req.method === 'POST') {
+    try {
+        const walletConnection = await rainbowKit.connect(); 
+      if (walletConnection) {
+        res.status(200).json({status: 'success', message: 'Wallet connected'});
+      } else {
+        res.status(400).json({status: 'failure', message: 'Could not connect wallet'});
+      }
+    } catch (error) {
+      res.status(500).json({status: 'error', message: `An error occurred: ${error.message}`});
+    }
+  } else {
+    res.status(405).json({status: 'error', message: 'Method not allowed'});
+  }
+};
